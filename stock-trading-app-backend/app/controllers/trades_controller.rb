3a9ef5dac8587461ce03b91c71fa77b1f64adaf0
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-09-06 12:59:24 +0800

    to_serialized_json works

--- stock-trading-app-backend/app/controllers/trades_controller.rb
+++ stock-trading-app-backend/app/controllers/trades_controller.rb
@@ -2,14 +2,14 @@ class TradesController < ApplicationController
 
   def index
     trades=Trade.all
-    render json: TradeSerializer.new(trades)
+    render json: TradeSerializer.new(trades).to_serialized_json
   end
 
   def create
     user=User.find_by(username: params[:username])
     stock=Stock.find_or_create_by(ticker: params[:ticker])
     trade = Trade.create(stock_id: stock.id,user_id:user.id,price:params[:price],direction:params[:direction],quantity:params[:quantity])
-    render json: TradeSerializer.new(trade)
+    render json: TradeSerializer.new(trade).to_serialized_json
   end
 
 end
