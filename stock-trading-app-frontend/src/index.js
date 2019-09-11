91191e2c3a721d5611471e3fbf7f4d7325a736a7
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-09-11 15:41:12 +0800

    Created object containing country names and corresponding codes

--- stock-trading-app-frontend/src/index.js
+++ stock-trading-app-frontend/src/index.js
@@ -635,7 +635,71 @@ function addArticlesToNewsbar(array){
   })
 }
 
-topNews()
+topNews() //initiate top news from the moment the page is opened
+
+let countries={
+  "United Arab Emirates":"ae",
+  "Argentina":"ar",
+  "Austria":"at",
+  "Australia":"au",
+  "Belgium":"be",,
+  "Bulgaria":"bg",
+  "Brazil":"br",
+  "Canada":"ca",
+  "Switzerland":"ch",
+  "China":"cn",
+  "Colombia":"co",
+  "Cuba":"cu",
+  "Czech Republic":"cz",
+  "Germany":"de",
+  "Egypt":"eg",
+  "France":"fr",
+  "United Kingdom":"gb",
+  "Greece":"gr",
+  "Hong Kong":"hk",
+  "Hungary":"hu",
+  "Indonesia":"id",
+  "Ireland":"ie",
+  "Israel":"il",
+  "India":"in",
+  "Italy":"it",
+  "Japan":"jp",
+  "South Korea":"kr",
+  "Lithuania":"lt",
+  "Latvia":"lv",
+  "Morocco":"ma",
+  "Mexico":"mx",
+  "Malaysia":"my",
+  "Nigeria":"ng",
+  "Netherlands":"nl",
+  "Norway":"no",
+  "New Zealand":"nz",
+  "Philippines":"ph",
+  "Poland":"pl",
+  "Portugal":"pt",
+  "Romania":"ro",
+  "Serbia":"rs",
+  "Russia":"ru",
+  "Saudi Arabia":"sa",
+  "Sweden":"se",
+  "Singapore":"sg",
+  "Slovenia":"si",
+  "Slovakia":"sk",
+  "Thailand":"th",
+  "Turkey":"tr",
+  "Taiwan":"tw",
+  "Ukraine":"ua",
+  "United States":"us",
+  "Venezuela":"ve",
+  "South Africa":"za"
+}
+
+let countryOptions=document.getElementById('country-select')
+for (key in countries){
+  countryOptions.innerHTML+=`
+  
+  `
+}
 
 document.getElementById('newsbar').addEventListener('submit',function(event){
   if(event.target.id==="topic-news"){
